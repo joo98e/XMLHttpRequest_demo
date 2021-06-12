9bc3f6fdd3118daced16b0e7fbe0b53d08eb9d3c
tae beok <joo98e@gmail.com>
2021-06-12 23:10:22 +0900

    json.parse() to add tags

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const TAGNAME_H1 = document.createElement('h1');
+const TAGNAME_H2 = document.createElement('h2');
+const TAGNAME_H3 = document.createElement('H3');
+const TAGNAME_H4 = document.createElement('H4');
+const TAGNAME_SPAN = document.createElement('SPAN');
+
 window.addEventListener('load', function () {
     
     const form = this.document.querySelector('#user-form');
@@ -18,8 +24,21 @@ const onNameSubmit = (e, a) => {
     API.onreadystatechange = e => {
         if(API.readyState === XMLHttpRequest.DONE){
             if(API.status == 200){
-                console.log(API.responseText);
-                console.log(yes);
+                let data = JSON.parse(API.response);
+                console.log(typeof data, data);
+
+                for (let key in data){
+                    try {
+                        const TAGNAME_P = document.createElement('P');
+                        TAGNAME_P.innerHTML = data[key];
+                        document.body.append(TAGNAME_P);
+                    } catch (error) {
+                        console.log('tag add error');
+                    }finally{
+                        console.log('parse is DONE');
+                    }
+                }
+
             }else{
                 console.log('error');
             }
