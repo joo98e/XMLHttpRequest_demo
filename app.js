51ef42b8841675d958c953e4e77904064ec27e72
tae beok <joo98e@gmail.com>
2021-06-12 23:29:37 +0900

    javascript is good

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const TAGNAME_H4 = document.createElement('H4');
 const TAGNAME_SPAN = document.createElement('SPAN');
 
 window.addEventListener('load', function () {
-    
+
     const form = this.document.querySelector('#user-form');
     let a = 1;
     form.addEventListener('submit', onNameSubmit, { capture: false, once: true, passive: false })
@@ -14,7 +14,7 @@ window.addEventListener('load', function () {
 
 const onNameSubmit = (e, a) => {
     e.preventDefault();
-    
+
     const API = new XMLHttpRequest();
 
     API.open('GET', './amIJSON.json', true);
@@ -22,26 +22,40 @@ const onNameSubmit = (e, a) => {
     API.send();
 
     API.onreadystatechange = e => {
-        if(API.readyState === XMLHttpRequest.DONE){
-            if(API.status == 200){
+        if (API.readyState === XMLHttpRequest.DONE) {
+            if (API.status == 200) {
                 let data = JSON.parse(API.response);
                 console.log(typeof data, data);
 
-                for (let key in data){
+                for (let key in data) {
                     try {
                         const TAGNAME_P = document.createElement('P');
                         TAGNAME_P.innerHTML = data[key];
                         document.body.append(TAGNAME_P);
                     } catch (error) {
                         console.log('tag add error');
-                    }finally{
-                        console.log('parse is DONE');
+                    } finally {
+                        
                     }
                 }
 
-            }else{
+            } else {
                 console.log('error');
             }
         }
+    }
+}
+
+const INT_MULTIPLY = x => x * x;
+
+class ADD_TAG {
+    constructor(name){
+        this.name = name;
+    }
+
+    ACTION_ADD = (text) => {
+        const tag = document.createElement(this.name);
+        tag.innerHTML = `추가될 텍스트는 ${text}입니다.`;
+        document.body.append(tag);
     }
 }
